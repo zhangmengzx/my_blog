0cc3fa2d632a4e126125e2b4ba04c0fd5aff5cec
zhangmeng <zhangmeng@nagain.com>
2017-12-25 15:58:19 +0800

    .

---
+++ blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,render_to_response
+
+# Create your views here.
+from blog.models import *
+from blog.forms import CommentForm
+from django.http import Http404
+
+
+def get_blogs(request):
+    blogs = Blog.objects.all().order_by('-created')
+    return render_to_response('blog_list.html', {'blogs': blogs})
+
+
+def get_details(request, blog_id):
+    try:
+        blog = Blog.objects.get(id=blog_id)
+    except Blog.DoesNotExist:
+        raise Http404
+    if request.method == 'GET':
+        form = CommentForm()
+    else:
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            cleaned_data['blog'] = blog
+            Comment.objects.create(**cleaned_data)
+    ctx = {
+        'blog': blog,
+        'comments': blog.comment_set.all().order_by('-created'),
+        'form': form
+    }
+    return render(request, 'blog_details.html', ctx)
